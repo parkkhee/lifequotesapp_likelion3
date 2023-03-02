5b5df8d5b6d6d485d9929881ef9e939c5169cac3
parkkhee <khp963@naver.com>
2023-03-02 13:25:28 +0900

    9단계까지 완료

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,10 @@
 package org.example;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class App {
+public class App{
     static ArrayList<Book> books = new ArrayList<>();
     public void run(){
         System.out.println("== 명언 앱 ==");
@@ -16,6 +17,27 @@ public class App {
             System.out.printf("명령) ");
             String line = sc.nextLine().trim();
             if (line.equals("종료")) {
+
+
+                try {
+                    File file = new File("C:/Users/kwan/Desktop/test/example.txt");
+                    // 파일 출력 스트림 생성
+                    FileOutputStream fos = new FileOutputStream(file);
+                    // PrintWriter로 출력 스트림 감싸기
+                    PrintWriter pw = new PrintWriter(fos);
+                    // ArrayList의 모든 요소를 파일에 쓰기
+                    for (Book book_list : books) {
+                        pw.println(book_list.getNo()+"/"+book_list.getAuthor()+"/"+book_list.getContent());
+                    }
+
+                    // 출력 스트림 닫기
+                    pw.close();
+                    fos.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 break;
             } else if (line.equals("등록")) {
 
@@ -28,6 +50,26 @@ public class App {
                 books.add(new Book(lq_no, author, lifequotes));
                 lq_no++;
             } else if (line.equals("목록")) {
+
+                if(books.isEmpty()){
+                    try {
+                        File file = new File("C:/Users/kwan/Desktop/test/example.txt");
+                        FileInputStream fis = new FileInputStream(file);
+                        // BufferedReader로 입력 스트림 감싸기
+                        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+                        // 파일의 모든 라인을 읽어서 ArrayList에 저장
+                        String str;
+                        while ((str = br.readLine()) != null) {
+                            String[] txt_in = str.split("/");
+                            books.add(new Book(Long.parseLong(txt_in[0]), txt_in[1], txt_in[2]));
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
                 System.out.println("------------------------------");
                 for (int i = books.size()-1; i >= 0; i--) {
                     Book now_book = books.get(i);
